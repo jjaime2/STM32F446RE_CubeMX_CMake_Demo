5fb1935b4fb6a6b3c465641c218c55a09085481d
Jose Jaime <jose33jaime@gmail.com>
2024-04-12 10:49:13 -0700

    Add SystemClock_Config and assert functionality

--- Inc/main.hpp
+++ Inc/main.hpp
@@ -1,1 +1,59 @@
 #pragma once
+
+#if defined(USE_FULL_ASSERT)
+#include "stm32_assert.h"
+#endif /* USE_FULL_ASSERT */
+
+#define LED1_PIN LL_GPIO_PIN_0
+#define LED1_GPIO_PORT GPIOB
+#define LED1_GPIO_CLK_ENABLE()                                                 \
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB)
+
+/**
+ * @brief LED2
+ */
+
+#define LED2_PIN LL_GPIO_PIN_7
+#define LED2_GPIO_PORT GPIOB
+#define LED2_GPIO_CLK_ENABLE()                                                 \
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB)
+
+/**
+ * @brief LED3
+ */
+
+#define LED3_PIN LL_GPIO_PIN_14
+#define LED3_GPIO_PORT GPIOB
+#define LED3_GPIO_CLK_ENABLE()                                                 \
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB)
+
+/**
+ * @brief Key push-button
+ */
+#define USER_BUTTON_PIN LL_GPIO_PIN_13
+#define USER_BUTTON_GPIO_PORT GPIOC
+#define USER_BUTTON_GPIO_CLK_ENABLE()                                          \
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC)
+#define USER_BUTTON_EXTI_LINE LL_EXTI_LINE_13
+#define USER_BUTTON_EXTI_IRQn EXTI15_10_IRQn
+#define USER_BUTTON_EXTI_LINE_ENABLE()                                         \
+  LL_EXTI_EnableIT_0_31(USER_BUTTON_EXTI_LINE)
+#define USER_BUTTON_EXTI_FALLING_TRIG_ENABLE()                                 \
+  LL_EXTI_EnableFallingTrig_0_31(USER_BUTTON_EXTI_LINE)
+#define USER_BUTTON_SYSCFG_SET_EXTI()                                          \
+  do {                                                                         \
+    LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);                      \
+    LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE13);      \
+  } while (0)
+#define USER_BUTTON_IRQHANDLER EXTI15_10_IRQHandler
+
+/* ==============   BOARD SPECIFIC CONFIGURATION CODE END      ============== */
+
+/**
+ * @brief Toggle periods for various blinking modes
+ */
+#define LED_BLINK_FAST 200
+#define LED_BLINK_SLOW 500
+#define LED_BLINK_ERROR 1000
+
+void SystemClock_Config();
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,18 +1,100 @@
 #include "main.hpp"
-#include "stm32f446xx.h"
-#include "stm32f4xx.h"
-
-#define PA5_OUTPUT (1 << 10)
-#define LED_PIN (1 << 5)
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_pwr.h"
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_system.h"
 
 int main() {
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-  GPIOA->MODER |= PA5_OUTPUT;
+  SystemClock_Config();
 
   while (true) {
-    GPIOA->ODR ^= LED_PIN;
-
-    for (volatile uint32_t i = 0; i < 100000; ++i)
-      ;
+    continue;
   }
 }
+
+/**
+ * System Clock Configuration
+ *
+ *   System Clock Source = PLL (HSE)
+ *   SYSCLK (Hz) = 180000000
+ *   HCLK (Hz) = 180000000
+ *   AHB Prescaler = 1
+ *   APB1 Prescaler = 4
+ *   APB2 Prescaler = 2
+ *   HSE Frequency (Hz) = 8000000
+ *   PLL_M = 8
+ *   PLL_N = 360
+ *   PLL_P = 2
+ *   VDD (V) = 3.3
+ *   Main regulator output voltage = Scale1 mode
+ *   Flash Latency (WS) = 5
+ */
+void SystemClock_Config() {
+  // Enable HSE Oscillator
+  LL_RCC_HSE_Enable();
+  while (LL_RCC_HSE_IsReady() != 1) {
+    continue;
+  }
+
+  // Set flash latency
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_5);
+
+  // Enable PWR clock
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+
+  // Activate Overdrive mode
+  LL_PWR_EnableOverDriveMode();
+  while (LL_PWR_IsActiveFlag_OD() != 1) {
+    continue;
+  }
+
+  // Active Overdrive switching
+  LL_PWR_EnableOverDriveSwitching();
+  while (LL_PWR_IsActiveFlag_ODSW() != 1) {
+    continue;
+  }
+
+  // Main PLL configuration and activation
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 360,
+                              LL_RCC_PLLP_DIV_2);
+  LL_RCC_PLL_Enable();
+  while (LL_RCC_PLL_IsReady() != 1) {
+    continue;
+  }
+
+  // Activate system clock on the main PLL
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) {
+    continue;
+  }
+
+  // Set APB1 and APB2 prescaler
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_4);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_2);
+
+  // Set systick to 1ms
+  SysTick_Config(180000000 / 1000);
+
+  // Update CMSIS SystemCoreClock variable
+  SystemCoreClock = 180000000;
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d", file,
+     line) */
+  /* Infinite loop */
+  while (1) {
+  }
+}
+#endif
